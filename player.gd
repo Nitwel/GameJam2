9dac0b22326b565e382751237185ba50bf026c0b
Nitwel <mail@nitwel.de>
2024-04-13 23:13:13 +0200

    fix

--- player.gd
+++ player.gd
@@ -12,6 +12,7 @@ func _physics_process(delta):
 	var direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 
 	if direction != Vector2.ZERO:
+		moved = true
 		velocity = direction * speed * delta
 		if direction.x > 0:
 			animation_player.play("walk_right")
