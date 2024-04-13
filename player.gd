dc2e0cd66726f5acca280c7b474dacae877cd241
PaulDeckert <p.dckrt@gmail.com>
2024-04-13 21:55:04 +0200

    [player] removed print

--- player.gd
+++ player.gd
@@ -11,7 +11,6 @@ func _physics_process(delta):
 	var direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 
 	if direction != Vector2.ZERO:
-		print(direction)
 		velocity = direction * speed * delta
 		if direction.x > 0:
 			animation_player.play("walk_right")
