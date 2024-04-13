2f4c522f70fc6faac048543b7e82b1a19e3329c7
Nitwel <mail@nitwel.de>
2024-04-13 16:04:29 +0200

    fix

--- main.gd
+++ main.gd
@@ -6,11 +6,11 @@ extends Node2D
 @onready var map = $Map
 
 func _ready():
-    Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func start_battle():
-    remove_child(player)
-    remove_child(map)
-    add_child(battle)
-    inventory.disabled = true
-    battle.start()
+	remove_child(player)
+	remove_child(map)
+	add_child(battle)
+	inventory.disabled = true
+	battle.start()
